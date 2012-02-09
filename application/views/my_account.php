2d9a55c2f9d04601f4eef203980c49765bd24b06
Rhys Williams <rrrhys@gmail.com>
2012-02-10 06:55:04 +1100

    create my account page, to do list

---
+++ application/views/my_account.php
@@ -0,0 +1,13 @@
+	<div class="span8 offset2">
+      <form class="form-horizontal">
+        <fieldset>
+          <legend><?=$title?></legend>
+          <table class="table table-striped">
+          	<tr>
+          		<td>Secret Key</td>
+          		<td><span id="secret_key"><?=$secret_key?></span> <a id="copy_secret_key">copy</a></td>
+          	</tr>
+          </table>
+         </fieldset>
+        </form>
+    </div>
